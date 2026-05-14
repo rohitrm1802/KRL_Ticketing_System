31886388f0fe48dc283ad6c7a1b1133d063b405b
Rohit <rohitrm1802@gmail.com>
2026-05-14 15:19:32 +0530

    Initial commit

---
+++ src/main/java/com/project/krl_ticketing_system/KrlTicketingSystemApplication.java
@@ -0,0 +1,13 @@
+package com.project.krl_ticketing_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KrlTicketingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KrlTicketingSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/krl_ticketing_system/KrlTicketingSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.krl_ticketing_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KrlTicketingSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
