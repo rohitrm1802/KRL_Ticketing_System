2071db533ee7fb4bfb3921604ac0f7ba89616d82
Kalyani <ganorkarkalyani5@gmail.com>
2026-05-19 14:26:14 +0530

    Added Show Entity

---
+++ src/main/java/com/project/krl_ticketing_system/Entity/Show.java
@@ -0,0 +1,19 @@
+package com.project.krl_ticketing_system.Entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class Show {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+
+    private String show_time;
+
+}
