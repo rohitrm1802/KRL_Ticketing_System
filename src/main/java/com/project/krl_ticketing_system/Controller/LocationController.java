de139081c1bf2bc0d327743d5a06395a3e66a582
Rohit <rohitrm1802@gmail.com>
2026-05-21 17:32:49 +0530

    work progress

--- src/main/java/com/project/krl_ticketing_system/Controller/LocationController.java
+++ src/main/java/com/project/krl_ticketing_system/Controller/LocationController.java
@@ -1,4 +1,34 @@
 package com.project.krl_ticketing_system.Controller;
 
+import com.project.krl_ticketing_system.Entity.Location;
+import com.project.krl_ticketing_system.Service.LocationService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/location")
 public class LocationController {
+
+    @Autowired
+    private LocationService locationService;
+
+    @PostMapping("/addLocation")
+    public String addLocation(@PathVariable String location)
+    {
+        return locationService.addLocation(location);
+    }
+
+    @GetMapping("/getLocation")
+    public List<Location> getLocation()
+    {
+        return locationService.getAllLocation();
+    }
+
+    @DeleteMapping("/deleteLocation")
+    public String deleteLocation(@PathVariable Long locationId)
+    {
+        return locationService.deleteLocation(locationId);
+    }
 }
